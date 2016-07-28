1848e5e1984c3e4df07ca7dbd465fc7c71b820c6
balibou <benjamin.cherion@gmail.com>
2016-07-28 15:59:41 +0100

    mockup template page

--- imports/ui/LandingPage.jsx
+++ imports/ui/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-const LOGO_URL = 'assets/images/logo.svg';
 const IPHONE_URL = 'assets/images/iphone.png';
 const TECHCRUNCH_URL = 'assets/images/press-01.jpg';
 const FORBES_URL = 'assets/images/press-02.jpg';
@@ -17,12 +16,14 @@ export default class LandingPage extends Component {
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
               </button>
-              <a className="logo" href="#"><img src={LOGO_URL} alt="Logo" /></a>
+              <div className="nav navbar-nav navbar-left">
+                <li><a className="logo" href="#">LIKER</a></li>
+              </div>
             </div>
             <div className="navbar-collapse collapse">
               <ul className="nav navbar-nav navbar-right">
-                <li><a href="#pricing" className="scroll">Pricing</a></li>
-                <li><a href="#">Sign in</a></li>
+                <li><a href="#pricing" className="scroll black">Pricing</a></li>
+                <li><a href="#about" className="scroll">About</a></li>
               </ul>
             </div>
           </div>
@@ -31,30 +32,28 @@ export default class LandingPage extends Component {
         <header>
           <div className="container">
             <div className="row">
-              <div className="col-xs-6">
-                <a href="#"><img src={LOGO_URL} alt="Logo" /></a>
+              <div className="col-xs-6 text-left navbar-nav">
+                <a className="logo" href="#">LIKER</a>
               </div>
               <div className="col-xs-6 signin text-right navbar-nav">
-                <a href="#pricing" className="scroll">Pricing</a>&nbsp; &nbsp;<a href="#">Sign in</a>
+                <a href="#pricing" className="scroll">Pricing</a>&nbsp;
+                &nbsp;<a href="#about" className="scroll">About</a>&nbsp;
               </div>
             </div>
 
             <div className="row header-info">
               <div className="col-sm-10 col-sm-offset-1 text-center">
-                <h1 className="wow fadeIn">A free landing page theme</h1>
+                <h1 className="wow fadeIn black">Make your Instagram account <br />SUPER</h1>
                 <br />
-                <p className="lead wow fadeIn" data-wow-delay="0.5s">Lorem ipsum dolor sit amet, consectetur adipis. <br/>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit sit dol mi arlna.</p>
+                <p className="lead wow fadeIn black" data-wow-delay="0.5s">Instagram auto-liker, auto-commenter.<br/>
+                Being famous has never been so easy !</p>
                 <br />
 
                 <div className="row">
                   <div className="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
                     <div className="row">
-                      <div className="col-xs-6 text-right wow fadeInUp" data-wow-delay="1s">
+                      <div className="col-xs-offset-3 col-xs-6 wow fadeInUp" data-wow-delay="1s">
                         <a href="#be-the-first" className="btn btn-secondary btn-lg scroll">Learn More</a>
-                      </div>
-                      <div className="col-xs-6 text-left wow fadeInUp" data-wow-delay="1.4s">
-                        <a href="#invite" className="btn btn-primary btn-lg scroll">Request Invite</a>
                       </div>
                     </div>
                   </div>
@@ -73,8 +72,8 @@ export default class LandingPage extends Component {
           <div className="container">
             <div className="row">
               <div className="col-sm-8 col-sm-offset-2 text-center margin-30 wow fadeIn" data-wow-delay="0.6s">
-                <h2>Be the first</h2>
-                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipis.</p>
+                <h2>Feel the magic</h2>
+                <p className="lead">Just connect, and see your Instagram account growing !</p>
               </div>
             </div>
 
@@ -87,20 +86,19 @@ export default class LandingPage extends Component {
         <section id="main-info" className="pad-xl">
     	    <div className="container">
     		    <div className="row">
-    			    <div className="col-sm-4 wow fadeIn" data-wow-delay="0.4s">
-    				    <hr className="line purple" />
-    				    <h3>App Feature One Here</h3>
-    				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut est facilisis, eu elementum mi volutpat. Pellentesque ac tristique nisi.</p>
+    			    <div className="col-sm-offset-2 col-sm-4 wow fadeIn" data-wow-delay="0.4s">
+    				    <hr className="line yellow" />
+    				    <h3>Auto-liker</h3>
+    				    <p>Liking your list of hashtags easily.<br />
+                The more you like instagram pictures, the more you get views from other instagramers !
+                </p>
     			    </div>
     			    <div className="col-sm-4 wow fadeIn" data-wow-delay="0.8s">
-    				    <hr  className="line blue" />
-    				    <h3>App Feature One Here</h3>
-    				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut est facilisis, eu elementum mi volutpat. Pellentesque ac tristique nisi.</p>
-    			    </div>
-    			    <div className="col-sm-4 wow fadeIn" data-wow-delay="1.2s">
     				    <hr  className="line yellow" />
-    				    <h3>App Feature One Here</h3>
-    				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut est facilisis, eu elementum mi volutpat. Pellentesque ac tristique nisi.</p>
+    				    <h3>Auto-commenter</h3>
+    				    <p>Commenting your list of hashtags easily.
+                Creating more interactions with instagramers will help them to know you better !
+                </p>
     			    </div>
     		    </div>
     	    </div>
@@ -111,47 +109,19 @@ export default class LandingPage extends Component {
             <div className="row margin-40">
               <div className="col-sm-8 col-sm-offset-2 text-center">
                 <h2 className="white">Pricing</h2>
-                <p className="white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.</p>
               </div>
             </div>
 
             <div className="row margin-50">
 
-              <div className="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="1s">
-                <br />
+              <div className="col-sm-offset-4 col-sm-4 pricing-container wow fadeInUp" data-wow-delay="0.4s">
                 <ul className="list-unstyled pricing-table text-center">
-        					<li className="headline"><h5 className="white">Personal</h5></li>
+        					<li className="headline"><h5 className="white">3 DAYS FREE TRIAL</h5></li>
         					<li className="price"><div className="amount">$5<small>/m</small></div></li>
-        					<li className="info">2 row section for you package information. You can include all details or icons</li>
-        					<li className="features first">Up To 25 Projects</li>
-        					<li className="features">10GB Storage</li>
-        					<li className="features">Other info</li>
-        					<li className="features last btn btn-secondary btn-wide"><a href="#">Get Started</a></li>
-        				</ul>
-              </div>
-
-              <div className="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="0.4s">
-                <ul className="list-unstyled pricing-table active text-center">
-        					<li className="headline"><h5 className="white">Professional</h5></li>
-        					<li className="price"><div className="amount">$12<small>/m</small></div></li>
-        					<li className="info">2 row section for you package information. You can include all details or icons</li>
-        					<li className="features first">Up To 25 Projects</li>
-        					<li className="features">10GB Storage</li>
-        					<li className="features">Other info</li>
-        					<li className="features">Other info</li>
-        					<li className="features last btn btn-secondary btn-wide"><a href="#">Get Started</a></li>
-        				</ul>
-              </div>
-
-              <div className="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="1.3s">
-                <br />
-                <ul className="list-unstyled pricing-table text-center">
-        					<li className="headline"><h5 className="white">Business</h5></li>
-        					<li className="price"><div className="amount">$24<small>/m</small></div></li>
-        					<li className="info">2 row section for you package information. You can include all details or icons</li>
-        					<li className="features first">Up To 25 Projects</li>
-        					<li className="features">10GB Storage</li>
-        					<li className="features">Other info</li>
+        					<li className="info">Instagram package</li>
+        					<li className="features first">1000 likes/day</li>
+        					<li className="features">100 comments/day</li>
+                  <li className="features">Set your own settings</li>
         					<li className="features last btn btn-secondary btn-wide"><a href="#">Get Started</a></li>
         				</ul>
               </div>
@@ -161,56 +131,38 @@ export default class LandingPage extends Component {
           </div>
         </section>
 
-        <section id="invite" className="pad-lg light-gray-bg">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-8 col-sm-offset-2 text-center">
-                <i className="fa fa-envelope-o margin-40"></i>
-                <h2 className="black">Get the invite</h2>
-                <br />
-                <p className="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.</p>
-                <br />
+        <section id="about" className="pad-xl">
+    	    <div className="container">
 
-                <div className="row">
-                  <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <form role="form">
-                      <div className="form-group">
-                        <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter Email" />
-                      </div>
-                      <button type="submit" className="btn btn-primary btn-lg">Request Invite</button>
-                    </form>
-                  </div>
-                </div>
-
-              </div>
+          <div className="row margin-40">
+            <div className="col-sm-8 col-sm-offset-2 text-center">
+              <h2 className="black">About</h2>
             </div>
           </div>
-        </section>
 
-        <section id="press" className="pad-sm">
-          <div className="container">
-
-            <div className="row margin-30 news-container">
-              <div className="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 wow fadeInLeft" data-wow-delay="0.8s">
-                <a href="#" target="_blank">
-                <img className="news-img pull-left" src={TECHCRUNCH_URL} alt="Tech Crunch" />
-                <p className="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.<br />
-                <small><em>Tech Crunch - January 15, 2015</em></small></p>
-                </a>
+    		    <div className="row">
+    			    <div className="col-sm-4 wow fadeIn" data-wow-delay="0.4s">
+    				    <hr className="line yellow" />
+    				    <h3>Security</h3>
+    				    <p>Transparency and privacy are the keys so you have the total control of your account preferences !<br />
+                You can manage your settings, your list of hashtags, the number of your likes and comments.
+                </p>
+    			    </div>
+    			    <div className="col-sm-4 wow fadeIn" data-wow-delay="0.8s">
+    				    <hr  className="line yellow" />
+    				    <h3>Payment</h3>
+    				    <p>For a maximum security, every payment are made throught Stripe.
+                </p>
+    			    </div>
+              <div className="col-sm-4 wow fadeIn" data-wow-delay="0.8s">
+                <hr  className="line yellow" />
+                <h3>Contact</h3>
+                <p>Don't hesitate to contact us for any question or advice.<br />
+                We are here to make you happy !
+                </p>
               </div>
-            </div>
-
-            <div className="row margin-30 news-container">
-              <div className="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 wow fadeInLeft" data-wow-delay="1.2s">
-                <a href="#" target="_blank">
-                <img className="news-img pull-left" src={FORBES_URL} alt="Forbes" />
-                <p className="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut. <br />
-                <small><em>Forbes - Feb 25, 2015</em></small></p>
-                </a>
-              </div>
-            </div>
-
-          </div>
+    		    </div>
+    	    </div>
         </section>
 
         <footer>
@@ -220,15 +172,12 @@ export default class LandingPage extends Component {
               <div className="col-sm-8 margin-20">
                 <ul className="list-inline social">
                   <li>Connect with us on</li>
-                  <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-                  <li><a href="#"><i className="fa fa-facebook"></i></a></li>
-                  <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                  <li><a href="#"><i className="fa fa-envelope"></i></a></li>
                 </ul>
               </div>
 
               <div className="col-sm-4 text-right">
-                <p><small>Copyright &copy; 2016. All rights reserved. <br />
-    	            Created by <a>Toto</a></small></p>
+                <p><small>Copyright Liker &copy; 2016. All rights reserved.</small></p>
               </div>
             </div>
 
